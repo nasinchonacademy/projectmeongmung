8c5c512dd377bdbe5972035678c23f5a3f1d2667
GiovanniBottesini <larkda2@nate.com>
2024-08-22 19:28:06 +0900

    Merge pull request #4 from nasinchonacademy/bottesini2
    Bottesini2

--- src/main/java/org/zerock/projectmeongmung/controller/SosBoardController.java
+++ src/main/java/org/zerock/projectmeongmung/controller/SosBoardController.java
@@ -4,43 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/soshospitallist")
 public class SosBoardController {
-
-    @GetMapping({"", "/"})
-    public String meongmung() {
-        return "mungSosHtml/soshospitallist"; // soshospitallist.html 템플릿을 반환
-    }
-
-    @GetMapping("/sosqnaboard")
-    public String sosqnaboard() {
-        return "mungSosHtml/sosqnaboard";
-    }
-
-    @GetMapping("/sosqnaedit")
-    public String sosqnaedit() {
-        return "mungSosHtml/sosqnaedit";
-    }
-
-    @GetMapping("/sosqnalist")
-    public String sosqnalist() {
-        return "mungSosHtml/sosqnalist";
-    }
-
-    @GetMapping("/sosqnawirte")
-    public String sosqnawirte() {
-        return "mungSosHtml/sosqnawirte";
-    }
-
-    @GetMapping("/vatprofileboard")
-    public String vatprofileboard() {
-        return "mungSosHtml/vatprofileboard";
-    }
-
-    @GetMapping("/vatprofilelist")
-    public String vatprofilelist() {
-        return "mungSosHtml/vatprofilelist";
-    }
 
 }
