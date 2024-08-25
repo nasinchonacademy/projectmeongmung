1e97a32b9c5e404c45cc389e54529b3c396d641d
nas <sktlf113@naver.com>
2024-08-25 18:01:08 +0900

    jeong 7:44 8/22

--- src/main/java/org/zerock/projectmeongmung/repository/MungStoryRepository.java
+++ src/main/java/org/zerock/projectmeongmung/repository/MungStoryRepository.java
@@ -1,4 +1,13 @@
 package org.zerock.projectmeongmung.repository;
 
-public interface MungStoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.zerock.projectmeongmung.entity.MungStory;
+import org.zerock.projectmeongmung.entity.User;
+
+import java.util.List;
+
+@Repository
+public interface MungStoryRepository extends JpaRepository<MungStory, Long> {
+    List<MungStory> findByUserId(Long userId);
 }
