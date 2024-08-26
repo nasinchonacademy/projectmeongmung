ffb764a92a0926dee4759982e36b6dee395b8810
GiovanniBottesini <larkda2@nate.com>
2024-08-26 11:51:00 +0900

    Merge remote-tracking branch 'origin/main' into bottesini

--- src/main/java/org/zerock/projectmeongmung/controller/MungStoryController.java
+++ src/main/java/org/zerock/projectmeongmung/controller/MungStoryController.java
@@ -1,4 +1,17 @@
 package org.zerock.projectmeongmung.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mungstory")
 public class MungStoryController {
+
+    // 기본적인 soshospitallist.html 템플릿을 반환
+    @GetMapping
+    public String soshospitallist() {
+        return "mungStoryHtml/storyboard";
+    }
+
 }
